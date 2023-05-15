a9fb415b9ee8166b4f5207e5fed6afbe5c02e34a
shir <shirfintsy@gmail.com>
2023-05-15 12:51:53 +0300

    changed for 4 requests

--- src/instructions/gameInstructions.js
+++ src/instructions/gameInstructions.js
@@ -25,7 +25,7 @@ function GameInstructions() {
       <h2 className={"app-heading-h2"} style={{"marginBottom": "20px"}}>About the task</h2>
       <p className={"instruction-passage"} hidden={partIdx !== 0}>
         <h5 style={{"color": "#db1111"}}>Please take good care, there will be an attention quiz later on!</h5>
-        Your task is to correctly classify 70 pictures of dogs and cats. At any given stage you will receive a
+        Your task is to correctly classify 80 pictures of dogs and cats. At any given stage you will receive a
           picture and you will need to press the button with the correct classification according to your best judgement.<br/>
         To enrich our pile of annotated pictures, we also have a robot and another user named Alex working on the same task
         in parallel (classifying a different set of pictures).<br/>
@@ -37,7 +37,7 @@ function GameInstructions() {
         <span style={{"fontWeight": "bold"}}>Helping the robot takes time as we need to save your current state and
           stream the robot's task instead (and then switch back). We estimate this interruption to take around 30-40
           seconds.<br/></span>You will not get score for identifying the robot's pictures.<br/>Once you finish correctly
-        classifying 70 pictures, we will ask for a short feedback.
+        classifying 80 pictures, we will ask for a short feedback.
       </p>
       <div className={"nav-btn-div"}>
         <Button hidden={partIdx === 0} onClick={onClickPrev}
